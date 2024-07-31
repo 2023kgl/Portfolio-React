af157bf0bbfda115f8544092b4a9fa9c69140a8d
Karina Gonzalez <karinagonzalez@Karinas-MacBook-Pro.local>
2024-07-30 18:08:59 -0600

    about

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,24 +1,18 @@
 import '../styles/Header.css'
-import { Link } from 'react-router-dom';
 import { Col, Row, Container } from 'react-bootstrap';
 import Navigation from './Navigation'
 
 function Header() {
     return (
         <>
-      <Container fluid className="bg-dark">
+
+      <Container fluid className="bg-white">
 
         <Row className="justify-content-center text-center mb-0">
 
           <Col className="pb-0">
-            <h1 className="text-white" style={{ fontSize: "70px" }}>
-              <Link
-                to=""
-                className="text-white text-decoration-none"
-                style={{ margin: "0" }}
-              >
-                Karina Gonzalez
-              </Link>
+            <h1 className="text-black" style={{ fontSize: "70px" }}>
+                Karina Gonzalez 💜
             </h1>
           </Col>
 
